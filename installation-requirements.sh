c714a68eac972fc7d59fb48eee0f2fe0d81f8c8e
AlexisEgea <alexisegea@outlook.com>
2024-08-03 17:52:07 +0200

    Adding instructions to run the project easily

---
+++ installation-requirements.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+echo "|                          Installation Requirements                        |"
+echo "| Author : Alexis EGEA                                                      |"
+echo "-----------------------------------------------------------------------------"
+
+echo "_____________________________________________________________________________"
+echo "creation venv repository..."
+python3 -m venv venv
+echo "...done"
+echo "_____________________________________________________________________________"
+echo "activation venv..."
+source venv/bin/activate #for linux
+#venv\Scripts\activate #for window
+echo "...done"
+echo "_____________________________________________________________________________"
+echo "installation requirement.txt"
+pip install -r requirement.txt
+echo "Done, project ready to be executed"
